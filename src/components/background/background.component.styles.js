b6a1f6854acba06d2f04872aeff30fcb1956e536
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-22 16:30:12 +0200

    perf(background component styles): add `will-change` property + clean unused properties

--- src/components/background/background.component.styles.js
+++ src/components/background/background.component.styles.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    //will-change: width, height, left, top;
-    //position: absolute;
-    //z-index: -1;
     background: ${(props) => props.theme.background};
     width: 100vw;
     height: 100vh;
     position: absolute;
+    will-change: width, height, left, top;
 `

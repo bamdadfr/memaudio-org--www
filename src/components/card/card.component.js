cabc8dba705d1a864e695999146cbcb76dfa736c
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-22 13:09:42 +0200

    feat: add flippable card

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Container } from './card.component.styles'
+import React, { useState } from 'react'
+import { useSpring } from '@react-spring/web'
+import { useMeasure } from 'react-use'
+import { Container, Card } from './card.component.styles'
 import { CardConstants } from './card.constants'
 
 /**
  * @param {object} props react props
- * @param {React.ReactElement} props.children content of the card
+ * @param {Array.<React.ReactElement>} props.children content of the card
  * @param {string} props.color color
  * @returns {React.ReactElement} react component
  */
@@ -13,10 +15,54 @@ export function CardComponent ({
     color = CardConstants.color,
 }) {
 
+    const front = children[0] ? children[0] : children
+    const back = children[1] ? children[1] : null
+    const [flipped, setFlipped] = useState (false)
+    const [ref, { width, height }] = useMeasure ()
+
+    const { transform, opacity } = useSpring ({
+        'opacity': flipped ? 1 : 0,
+        'transform': `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+        'config': { 'mass': 10, 'tension': 500, 'friction': 80 },
+    })
+
     return (
         <>
-            <Container color={color}>
-                {children}
+            <Container
+                ref={ref}
+                tabIndex={-1}
+                role="button"
+            >
+                <Card
+                    onClick={() => setFlipped ((f) => !f)}
+                    color={color}
+                    width={width}
+                    height={height}
+                    style={{
+                        'opacity': opacity.to ((o) => 1 - o),
+                        transform,
+                    }}
+                >
+                    {front}
+                </Card>
+                {back
+                    ?
+                        <Card
+                            onClick={() => setFlipped ((f) => !f)}
+                            color={color}
+                            width={width}
+                            height={height}
+                            style={{
+                                opacity,
+                                transform,
+                                'rotateY': '180deg',
+                            }}
+                        >
+                            {back}
+                        </Card>
+                    : null
+                }
+
             </Container>
         </>
     )
--- src/components/card/card.component.styles.js
+++ src/components/card/card.component.styles.js
@@ -1,17 +1,36 @@
 import styled from 'styled-components'
+import { a } from '@react-spring/web'
 
 export const Container = styled.div`
-    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     height: 100%;
+    width: 100%;
+`
+
+export const Card = styled (a.div)`
+    position: fixed;
+    width: ${(props) => props.width}px;
+    height: ${(props) => props.height}px;
+    will-change: transform, opacity;
+    background: blue;
 
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
 
     color: ${(props) => props.theme.background};
     background: ${(props) => props.color};
 
+    backface-visibility: hidden;
+
     &:hover {
         cursor: pointer;
+    }
+
+    > * {
+        display: flex;
     }
 `

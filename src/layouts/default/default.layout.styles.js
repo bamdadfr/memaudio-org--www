104ca334776a3237a4c568a57b05428815332277
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-22 13:08:14 +0200

    fix: set correct font size and derive svg size from it

--- src/layouts/default/default.layout.styles.js
+++ src/layouts/default/default.layout.styles.js
@@ -16,33 +16,41 @@ export const Container = styled.div`
 `
 
 export const Wrapper = styled.div`
+    * {
+        user-select: none;
+    }
+
+    svg {
+        font-size: 5em;
+    }
+
     @media screen and ${Breakpoints.mobile} {
         width: 24rem;
         height: 24rem;
-        font-size: 6em;
+        font-size: 1.2em;
     }
 
     @media screen and ${Breakpoints.tablet} {
         width: 32rem;
         height: 32rem;
-        font-size: 10em;
+        font-size: 1.4em;
     }
 
     @media screen and ${Breakpoints.desktop} {
         width: 36rem;
         height: 36rem;
-        font-size: 12em;
+        font-size: 1.6em;
     }
 
     @media screen and ${Breakpoints.widescreen} {
         width: 38rem;
         height: 38rem;
-        font-size: 13em;
+        font-size: 1.8em;
     }
 
     @media screen and ${Breakpoints.fullhd} {
         width: 42rem;
         height: 42rem;
-        font-size: 15em;
+        font-size: 2em;
     }
 `

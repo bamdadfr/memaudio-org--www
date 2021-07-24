41371be22d06098480d1ae620754d467b3fe6366
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-24 11:27:48 +0200

    feat(grid.component): add transition animation

--- src/components/grid/grid.component.js
+++ src/components/grid/grid.component.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Grid } from './grid.component.styles'
+import React, { useEffect, useState } from 'react'
+import { animated, useTransition } from '@react-spring/web'
+import { useMeasure } from 'react-use'
+import { Container, Grid } from './grid.component.styles'
 import { useGridComponent } from './hooks'
 
 /**
@@ -10,12 +12,41 @@ import { useGridComponent } from './hooks'
 export function GridComponent ({ children }) {
 
     const { columns, rows } = useGridComponent (children.length)
+    const [items, setItems] = useState ([])
+    const [ref, { width }] = useMeasure ()
+
+    const transition = useTransition (items, {
+        'from': { 'opacity': 0, 'x': width * 2 * -1 },
+        'enter': { 'opacity': 1, 'x': 0 },
+        'leave': { 'opacity': 0, 'x': width * 2 },
+        'config': { 'mass': 5, 'tension': 500, 'friction': 100 },
+        'trail': 75,
+    })
+
+    useEffect (() => {
+
+        setTimeout (() => {
+
+            setItems (children)
+
+        }, 0)
+
+    }, [])
 
     return (
         <>
-            <Grid columns={columns} rows={rows}>
-                {children}
-            </Grid>
+            <Container ref={ref}>
+                <Grid columns={columns} rows={rows}>
+                    {transition ((style, item) => (
+                        item
+                            ?
+                                <animated.div style={style}>
+                                    {item}
+                                </animated.div>
+                            : ''
+                    ))}
+                </Grid>
+            </Container>
         </>
     )
 

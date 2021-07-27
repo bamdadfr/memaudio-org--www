d784d95b8d1c30a5c997609527f6d1c38f4ca115
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-27 02:37:12 +0200

    fix(utils): add `undefined` case in capitalize-first-letter.js

--- src/utils/capitalize-first-letter.js
+++ src/utils/capitalize-first-letter.js
@@ -1,8 +1,10 @@
 /**
- * @param {string} string lowercase
- * @returns {string} capitalized
+ * @param {string|undefined} [string] lowercase
+ * @returns {string|void} capitalized
  */
 export function capitalizeFirstLetter (string) {
+
+    if (typeof string === 'undefined') return
 
     return string.charAt (0).toUpperCase () + string.slice (1)
 

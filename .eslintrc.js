dc0462f8fe8566ec903099bf6226a45c278ffa6a
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-28 12:44:34 +0200

    perf(deps): update to latest eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
     'extends': '@bamdadsabbagh/eslint-config',
     'plugins': [
-        'react-hooks',
         'testing-library',
         'jest-dom',
     ],
-    'rules': {
-        'react-hooks/rules-of-hooks': ['error'],
-        'react-hooks/exhaustive-deps': ['warn'],
-    },
 }

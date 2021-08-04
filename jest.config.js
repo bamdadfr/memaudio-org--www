6eaa2d03c23c3c225696000a76dff5ffcfe2ffae
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-04 16:22:34 +0200

    perf(tests): update jest.config.js + add global mock for `next/head`

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
     'setupFilesAfterEnv': ['./jest.setup.js'],
-    'testRegex': '(test|spec)\\.(.*\\.)?[jt]sx?$',
+    'testRegex': '(test|spec)\\.[jt]sx?$',
     'moduleNameMapper': {
         '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        'next/head': '<rootDir>/__mocks__/next-head.js',
     },
     'testEnvironment': 'jsdom',
     'modulePathIgnorePatterns': [
         '<rootDir>/.next/',
     ],
-    'collectCoverage': true,
     'coverageDirectory': './coverage/',
+    'collectCoverageFrom': [
+        '<rootDir>/src/**/*.js',
+    ],
 }

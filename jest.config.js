761cdc47f804252fc1f9c246ff6a00240951772a
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-04 16:30:05 +0200

    perf(deps): remove unused `jest-transform-stub`

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
     'setupFilesAfterEnv': ['./jest.setup.js'],
     'testRegex': '(test|spec)\\.[jt]sx?$',
     'moduleNameMapper': {
-        '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
         'next/head': '<rootDir>/__mocks__/next-head.js',
     },
     'testEnvironment': 'jsdom',

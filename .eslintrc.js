1eac5f145b633670023d70af27631650d3cc031f
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-05 13:47:56 +0200

    chore(linting): replace babel parser with typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     'extends': '@bamdadsabbagh/eslint-config',
+    'parser': '@typescript-eslint/parser',
     'plugins': [
         'testing-library',
         'jest-dom',
     ],
+    'rules': {
+        'no-use-before-define': 'off',
+    },
 }

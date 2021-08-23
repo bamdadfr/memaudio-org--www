fabf1658de73dc3d947d81d45a9e1ec2247e7037
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-24 01:10:16 +0200

    fix(worlds): fix paths for world `synths_chords_2021`

--- src/app/data/files/synths_chords_2021/synths_chords_2021.js
+++ src/app/data/files/synths_chords_2021/synths_chords_2021.js
@@ -1,12 +1,14 @@
-export const instruments_kick = {
-    'synths_chords_2021_001': '/files/instruments_kick/kick_-001.mp3',
-    'synths_chords_2021_002': '/files/instruments_kick/kick_-002.mp3',
-    'synths_chords_2021_003': '/files/instruments_kick/kick_-003.mp3',
-    'synths_chords_2021_004': '/files/instruments_kick/kick_-004.mp3',
-    'synths_chords_2021_005': '/files/instruments_kick/kick_-005.mp3',
-    'synths_chords_2021_006': '/files/instruments_kick/kick_-006.mp3',
-    'synths_chords_2021_007': '/files/instruments_kick/kick_-007.mp3',
-    'synths_chords_2021_008': '/files/instruments_kick/kick_-008.mp3',
-    'synths_chords_2021_009': '/files/instruments_kick/kick_-009.mp3',
-    'synths_chords_2021_010': '/files/instruments_kick/kick_-010.mp3',
+const basePath = '/files/synths_chords_2021/'
+
+export const synths_chords_2021 = {
+    '001': `${basePath}-001.mp3`,
+    '002': `${basePath}-002.mp3`,
+    '003': `${basePath}-003.mp3`,
+    '004': `${basePath}-004.mp3`,
+    '005': `${basePath}-005.mp3`,
+    '006': `${basePath}-006.mp3`,
+    '007': `${basePath}-007.mp3`,
+    '008': `${basePath}-008.mp3`,
+    '009': `${basePath}-009.mp3`,
+    '010': `${basePath}-010.mp3`,
 }

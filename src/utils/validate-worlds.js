a15c752b844b8110573b8765c853ca356c7f071c
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-24 00:22:50 +0200

    fix(validateWorlds): remove files validation to keep worlds and levels validation only (closes #164)

--- src/utils/validate-worlds.js
+++ src/utils/validate-worlds.js
@@ -1,11 +1,9 @@
 // noinspection RedundantIfStatementJS
 
 import { worlds } from '../app/data/worlds/worlds'
-import { files } from '../app/data/files/files'
 
 /**
- * @description checks for existence of worlds and files
- *      then checks if there is enough files for the requested level
+ * @description checks for existence of worlds and levels
  * @param {string} world slug
  * @param {string} level slug
  * @returns {boolean} world and level isValid?
@@ -14,22 +12,7 @@ export function validateWorlds (world, level) {
 
     if (typeof worlds[world] === 'undefined') return false
 
-    if (typeof files[world] === 'undefined') return false
-
     if (typeof worlds[world][level] === 'undefined') return false
-
-    const availableFiles = Object.keys (files[world]).length
-
-    const requestedFiles = worlds[world][level].reduce ((previous, current) => {
-
-        const newPrevious = typeof previous === 'number' ? previous : 1
-        const newCurrent = typeof current === 'number' ? current : 1
-
-        return newPrevious + newCurrent
-
-    })
-
-    if (requestedFiles > availableFiles) return false
 
     return true
 

d7ceba1923618cb65986dc0fa6df2386525bae1e
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-24 17:27:10 +0200

    perf(files): add test to check for each file entry validity

--- src/app/data/files/files.test.js
+++ src/app/data/files/files.test.js
@@ -1,11 +1,33 @@
+import fs from 'fs'
 import { files } from './files'
 
 describe ('files', () => {
 
-    it ('should be defined', () => {
+    it ('should contain valid entries', () => {
 
-        expect (files).toBeDefined ()
-    
+        const entries = Object.entries (files)
+        let invalidEntries = []
+
+        entries.forEach ((entry) => {
+
+            const filesObject = entry[1]
+            const filesEntries = Object.entries (filesObject)
+
+            filesEntries.forEach ((file) => {
+
+                const path = `./public${file[1]}`
+                const doesExist = fs.existsSync (path)
+
+                if (!doesExist) invalidEntries = [...invalidEntries, path]
+
+            })
+
+        })
+
+        if (invalidEntries.length !== 0) throw new Error (invalidEntries)
+
+        expect (invalidEntries.length).toBe (0)
+
     })
 
 })

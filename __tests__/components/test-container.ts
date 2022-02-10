00731123638fa4fa782bd32fb28a78c824ad1642
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-10 02:46:39 +0100

    chore(Tests): Avoid renaming render import

--- __tests__/components/test-container.ts
+++ __tests__/components/test-container.ts
@@ -1,8 +1,8 @@
-import {render as defaultRender} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import {ReactElement} from 'react';
 
-const render = (Component) => {
-  const {container} = defaultRender(Component);
+const r = (Component) => {
+  const {container} = render(Component);
 
   return {
     container,
@@ -12,13 +12,13 @@ const render = (Component) => {
 export const testContainer = (C: ReactElement): void => {
   describe('container', () => {
     it('should be defined and visible', () => {
-      const {container} = render(C);
+      const {container} = r(C);
       expect(container).toBeInTheDocument();
       expect(container).toBeVisible();
     });
 
     it('should not be empty', () => {
-      const {container} = render(C);
+      const {container} = r(C);
       expect(container).not.toBeEmptyDOMElement();
     });
   });

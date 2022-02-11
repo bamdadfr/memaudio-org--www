fa7aa2737ad2c7f849bf644ad7e9ff07caf385dd
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-11 12:17:50 +0100

    chore(Tests): Provide dummy audio files for `AudioAnnouncerComponent`

--- src/components/audio-announcer/audio-announcer.component.test.tsx
+++ src/components/audio-announcer/audio-announcer.component.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {AudioAnnouncerComponent} from './audio-announcer.component';
-import {announcer} from '../../app/data/announcer/announcer';
 import {testContainer} from '../../../__tests__/components/test-container';
+
+const audioFiles = [
+  'http://localhost/audio-01.mp3',
+  'http://localhost/audio-02.mp3',
+];
 
 describe('AudioAnnouncerComponent', () => {
   testContainer(
     <AudioAnnouncerComponent
-      files={[announcer.game.start]}
+      files={audioFiles}
     />,
   );
 });

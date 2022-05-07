a1880bd3acb6a1ab34c118154c9f7c6529f5e631
Bamdad Sabbagh <hi@bamdad.fr>
2022-05-07 12:20:01 +0200

    feat: Add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+/* eslint-disable jsdoc/valid-types */
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://www.memaudio.org/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
